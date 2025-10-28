f244bfef3424e74b06904e8884cc94f5b74cc7b4
oskar <iksoski2@gmail.com>
2025-10-28 22:06:22 +0100

    Update 28.10

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-#Analiza_Sprzedarzy
+Tytuł i opis:
+ANALIZA SPRZEDAZY
+Analizujemy sprzedaż firmy "SuperStore"
+
+Cel:
+Które produkty generują największy zysk, a które są nierentowne?
+Jakie są trendy sprzedaży?
+Czy firma osiąga swoje zyski?
+Jaki segment klientów jest najbardziej dochodowy?
+Które regiony mają najwiekszą/najniższą sprzedaż i zysk?
+
+Użyte narzędzia:
+Python(pandas,matplotlib)
+SQL(PostgreSQL)
+Git/Github
+
+Struktura projektu:
+Kod znajdziemy w folderze src/analiza.py
+
--- src/Analiza.py
+++ src/Analiza.py
@@ -1,13 +1,20 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 
-#Pobieramy do kodu csv
-df = pd.read_csv('/home/oskar/PycharmProjects/AnalizaSprzedazy/Data/Superstore Dataset.csv')
+CSV_FILE = '/home/oskar/PycharmProjects/AnalizaSprzedazy/Data/Superstore Dataset.csv'
+DATABASE_NAME = 'superstore.db'
+TABLE_NAME = 'sales_data'
 
-#Drukujemy jego informacje
-print(df.head())
-print(df.info())
-print(df.describe())
+# 1. Ładowanie i czyszczenie
+df = pd.read_csv(CSV_FILE, encoding='latin1')
+df.columns = df.columns.str.replace(r'[^A-Za-z0-9_]+', '', regex=True).str.strip().str.replace(' ', '_')
+# Konwersja numeryczna aby SQL działał poprawnie
+df['Sales'] = df['Sales'].astype(str).str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype(float)
+df['Profit'] = df['Profit'].astype(str).str.replace('$', '', regex=False).str.replace(',', '', regex=False).astype(float)
 
-#Ustawiamy w csv kolumny z datą żeby odczytywały jako date a nie jako text
-df['Order date']=pd.to_datetime(df['Order date'])
+# 2. Tworzenie bazy (plik) i ładowanie danych
+# 'sqlite:///superstore.db' informuje SQLAlchemy, że ma użyć SQLite i utworzyć plik.
+engine = create_engine(f'sqlite:///{DATABASE_NAME}')
+df.to_sql(TABLE_NAME, engine, if_exists='replace', index=False)
+
+print(f"Gotowe. Plik bazy danych '{DATABASE_NAME}' został utworzony!")
